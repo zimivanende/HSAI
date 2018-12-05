86b022a828f6413dfd1607ce1249e9489fafbe04
EagleEye <white_eclipse_1903>
2018-12-05 20:26:34 +0100

    profile page swipe views

--- programma/app/src/main/java/hsai/prototype/fietsveilig/ProfileActivity.java
+++ programma/app/src/main/java/hsai/prototype/fietsveilig/ProfileActivity.java
@@ -1,21 +1,129 @@
 package hsai.prototype.fietsveilig;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 
-public class ProfileActivity extends AppCompatActivity {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.Vector;
+
+public class ProfileActivity extends FragmentActivity {
+    private ViewPager m_pages;
+    private PageAdapter m_pageAdapter;
+    private TabLayout m_tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+        // get the page adapter that is going to provide the profile activity with its tab pages
+        m_pageAdapter = new PageAdapter(getSupportFragmentManager());
 
+        // set m_pageAdapter as our viewPager's adapter
+        m_pages = (ViewPager) findViewById(R.id.pager);
+        m_pages.setAdapter(m_pageAdapter); // now the adapter is going to provide the pages to the view
+
+        // add tabs
+        m_tabs = (TabLayout) findViewById(R.id.tabLayout);
+        m_tabs.setupWithViewPager(m_pages);
     }
+
+
+
+    // ############################################# begin page fragments #############################################
+
+    public static class ProgressFragment extends Fragment{
+        private static final String m_title = "Progress";
+
+        /**
+         * When fragment is being created
+         */
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+        }
+
+        /**
+         * The progress fragment's UI
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.profile_page_progress_fragment, container, false);
+        }
+
+        public static String getTitle(){
+            return m_title;
+        }
+    }
+
+
+
+    public static class FriendsFragment extends Fragment{
+        private static final String m_title = "Friends";
+
+        /**
+         * When fragment is being created
+         */
+        public void onCreate(Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+        }
+
+        /**
+         * The friend fragment's UI
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.profile_page_friends_fragment, container, false);
+        }
+
+        public static String getTitle(){
+            return m_title;
+        }
+    }
+
+    /**
+     * Contains the pages inside the profile activity the user can swipe:
+     *      - page 1: progress
+     *      - page 2: friends
+     */
+    class PageAdapter extends FragmentPagerAdapter{
+        private final int NUMPAGES = 2;
+        private Vector<Fragment> m_fragments = new Vector<>();
+        private Vector<String> m_fragmentsTitles = new Vector<>();
+
+
+        public PageAdapter(FragmentManager fm) {
+            super(fm);
+            m_fragments.add(new ProgressFragment());
+            m_fragmentsTitles.add("Progress");
+
+            m_fragments.add(new FriendsFragment());
+            m_fragmentsTitles.add("Friends");
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            return m_fragments.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return NUMPAGES;
+        }
+
+        public CharSequence getPageTitle(int position){
+            return m_fragmentsTitles.get(position);
+        }
+    }
+    // ############################################# end page fragments #############################################
+
 }
