b99d9fe74b3ee8ce90547defef5869126261774f
Zimi Van Ende <zvanende@gmail.com>
2018-12-12 11:30:25 +0100

    Cleaned

--- programma/app/src/main/java/hsai/prototype/fietsveilig/HomeScreen.java
+++ programma/app/src/main/java/hsai/prototype/fietsveilig/HomeScreen.java
@@ -22,7 +22,7 @@ public class HomeScreen extends AppCompatActivity {
        m_button.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               startActivity(new Intent(HomeScreen.this, ChooseTheoryDifficulty.class));
+               startActivity(new Intent(HomeScreen.this, TheoryScreen.class));
            }
        });
 
